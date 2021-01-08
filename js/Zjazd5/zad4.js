03605e3e37a86e242f9a018d6b305e793e0cd1fd
Patryk Klimek <s20158@pjwstk.edu.pl>
2021-01-08 21:01:35 +0100

    Added exerceices 3 and 4

--- js/Zjazd5/zad4.js
+++ js/Zjazd5/zad4.js
@@ -1,25 +1,13 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-//
-// $(function (){
-//     for (i = 0; i < acc.length; i++) {
-//         acc[i].addEventListener("click", function() {
-//             this.classList.toggle("active");
-//             var panel = this.nextElementSibling;
-//             if (panel.style.display === "block") {
-//                 panel.style.display = "none";
-//             } else {
-//                 panel.style.display = "block";
-//             }
-//         });
-//     }
-// })
+function I(a){
+    var x=document.getElementsByClassName("accordion_down")[a];
+    var a=document.getElementsByClassName("accordion_down");
 
-$(function() {
-
-    // var allPanels = $('.panel').hide();
-    $('.accordion').click(function() {
-        $(".panel").slideUp();
-        $(this).parent().next().slideDown();
-    });
-});
+    for(var i =0;i<a.length;i++){
+        a[i].style.display="none";
+    }
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
