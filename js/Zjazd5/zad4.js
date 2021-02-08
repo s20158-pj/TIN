f5f132a40abda06565a284263956a5b07f8a7ddc
Patryk Klimek <s20158@pjwstk.edu.pl>
2021-02-08 16:26:43 +0100

    Save changes after semester

--- js/Zjazd5/zad4.js
+++ js/Zjazd5/zad4.js
@@ -1,9 +1,9 @@
 function I(a){
     var x=document.getElementsByClassName("accordion_down")[a];
-    var a=document.getElementsByClassName("accordion_down");
+    var arr=document.getElementsByClassName("accordion_down");
 
-    for(var i =0;i<a.length;i++){
-        a[i].style.display="none";
+    for(var i =0;i<arr.length;i++){
+        arr[i].style.display="none";
     }
     if (x.style.display === "none") {
         x.style.display = "block";
